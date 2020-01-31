86b08c3781e48e1c9f0c8186c9b97f4a79245062
ShaquilleClarke <github email address>
2020-01-31 18:52:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    contentLoaded()
+});
+
+
+let textTarget = document.querySelector("#text")
+
+
+function contentLoaded() {
+    textTarget.innerHTML = "This is really cool!"
+}
+
+
+
+
+
+  
